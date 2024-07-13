f5c6a05dcb65852ca996006ea9926467933f1363
Dogukan CIFCI <107485142+DogukanCifci@users.noreply.github.com>
2024-07-13 17:10:09 +0200

    Update Users.jsx

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -19,52 +19,10 @@ const Users = () => {
   };
   return (
     <div>
-      <div className="">
-        {dataa.map((eleman) => {
-          const { id, name, email, phone, location, picture, registered, dob } =
-            eleman;
-          const { title, first, last } = name;
-          const { city, country } = location;
-          const { large } = picture;
-          console.log(eleman);
-          return (
-            <div className="container text-center" key={id}>
-              <div className="kapsayici baslik">
-                <img src={large} alt="" className="usersPhoto" />
-              </div>
-              <div className="kapsayici names">
-                <h5>
-                  {title} {first} {last}
-                </h5>
-              </div>
-              <div className="kapsayici mail">
-                <img src={emailImage} alt="" />
-              </div>
-              <div className="kapsayici">{email}</div>
-              <div className="kapsayici tel">
-                <img src={phoneImage} alt="" />
-              </div>
-              <div className="kapsayici">{phone}</div>
-              <div className="kapsayici location">
-                <img src={locationImage} alt="" />
-              </div>
-              <div className="kapsayici">
-                {city} {country}{" "}
-              </div>
+     <div>WARNING!!!!</div>
+      <div class="warning">Kisilel Bilgileriniz ve Adresiniz ele gecirilmistir!!</div>
+      <div>Tarayiciyi kapat!</div>
 
-              <div className="footer text-center">
-                <p>Age : {dob.age} </p>
-                <p>Register Date : {registered.date}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className="button text-center mt-4">
-        <button className="btn text-center mx-auto" onClick={newUser}>
-          Random User
-        </button>
-      </div>
     </div>
   );
 };
